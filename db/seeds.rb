8e9abb817f0d6ee00ed3e38dd8e5c79abf386992
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-08-16 15:46:56 -0400

    Seed data update for exercises

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-User.destroy_all
-Exercise.destroy_all
-Routine.destroy_all
-GroupClass.destroy_all
+# User.destroy_all
+# Exercise.destroy_all
+# Routine.destroy_all
+# GroupClass.destroy_all
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
@@ -16,10 +16,35 @@ GroupClass.destroy_all
 User.create(name: "Max", age: 22, weight: 185, weight_goal: 200, height_inch: 72, sex: "m")
 User.create(name: "Alice", age: 29, weight: 150, weight_goal: 120, height_inch: 63, sex: "f")
 
-Exercise.create(type_of: "idk", name: "burpees", youtube_link: "some link", description: "description", barbell?: false, weight: 0)
-Exercise.create(type_of: "idk", name: "push-ups", youtube_link: "some link", description: "description", barbell?: false, weight: 0)
-Exercise.create(type_of: "idk", name: "overhead claps", youtube_link: "some link", description: "description", barbell?: false, weight: 0)
-Exercise.create(type_of: "idk", name: "mf", youtube_link: "some link", description: "description", barbell?: false, weight: 0)
+# Exercise.create(type_of: "idk", name: "burpees", youtube_link: "some link", description: "description", barbell?: false, weight: 0)
+# Exercise.create(type_of: "idk", name: "push-ups", youtube_link: "some link", description: "description", barbell?: false, weight: 0)
+# Exercise.create(type_of: "idk", name: "overhead claps", youtube_link: "some link", description: "description", barbell?: false, weight: 0)
+# Exercise.create(type_of: "idk", name: "mf", youtube_link: "some link", description: "description", barbell?: false, weight: 0)
+
+
+
+
+Exercise.create(type_of: "chest", name: "bench press", youtube_link: "URL", description: "description", barbell?: true, weight: 0)
+Exercise.create(type_of: "chest", name: "push-ups", youtube_link: "URL", description: "description", barbell?: false, weight: 0)
+Exercise.create(type_of: "chest", name: "fly", youtube_link: "URL", description: "description", barbell?: false, weight: 0)
+Exercise.create(type_of: "full body", name: "squat", youtube_link: "URL", description: "description", barbell?: true, weight: 0)
+Exercise.create(type_of: "full body", name: "deadlift", youtube_link: "URL", description: "description", barbell?: true, weight: 0)
+Exercise.create(type_of: "back", name: "seated row", youtube_link: "URL", description: "description", barbell?: false, weight: 0)
+Exercise.create(type_of: "back", name: "barbell row", youtube_link: "URL", description: "description", barbell?: true, weight: 0)
+Exercise.create(type_of: "shoulder", name: "overhead press", youtube_link: "URL", description: "description", barbell?: true, weight: 0)
+Exercise.create(type_of: "shoulder", name: "shrugs", youtube_link: "URL", description: "description", barbell?: true, weight: 0)
+Exercise.create(type_of: "arm", name: "curls", youtube_link: "URL", description: "description", barbell?: false, weight: 0)
+Exercise.create(type_of: "arm", name: "tricep extensions", youtube_link: "URL", description: "description", barbell?: false, weight: 0)
+Exercise.create(type_of: "arm", name: "pull-ups", youtube_link: "URL", description: "description", barbell?: false, weight: 0)
+Exercise.create(type_of: "leg", name: "forward lunge", youtube_link: "URL", description: "description", barbell?: false, weight: 0)
+Exercise.create(type_of: "leg", name: "calf raises", youtube_link: "URL", description: "description", barbell?: false, weight: 0)
+Exercise.create(type_of: "abs", name: "crunches", youtube_link: "URL", description: "description", barbell?: false, weight: 0)
+Exercise.create(type_of: "abs", name: "sit-ups", youtube_link: "URL", description: "description", barbell?: false, weight: 0)
+
+
+
+
+
 
 Routine.create(name: "monday", user_id: 1, exercise_id: 2)
 Routine.create(name: "tuesday", user_id: 2, exercise_id: 4)
