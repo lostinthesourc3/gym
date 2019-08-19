caaa778a0c78524398aa29655106002e7049de14
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-08-19 09:30:16 -0400

    Create new user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,25 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         @user_routines = Routine.all
     end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(user_params)
+        redirect_to user_path(@user)
+    end
+
+    # private methods
+    private
+    def user_params
+        params.require(:user).permit(:name, :age, :height, :weight, :weigt_goal, :sex)
+    end
 end
+
+
+# def new
+#     @heroine = Heroine.new
+#     @powers = Power.all
+# end
