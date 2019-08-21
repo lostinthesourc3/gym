b4e0cb12d22b0361733df1265eb65a92ed3429b2
mattlistor <mattlistor@gmail.com>
2019-08-21 17:45:44 -0400

    barbell

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -11,14 +11,36 @@ class ExercisesController < ApplicationController
     end
 
     def weight
-        byebug
+        #byebug
         @exercise = Exercise.find(params[:id])
-        @exercise.weight = params[:weight]
-        redirect_to :weight
+        @exercise.update(weight: params[:weight])
+        @plate_hash = barbell(@exercise.weight)
+        render :weight
     end
 
     private
     def find_exercise
         @exercise = Exercise.find(params[:id])
+    end
+
+    def barbell(input)
+        if input.to_i == 45 
+            return "You only need the bar!"
+        elsif input.to_i < 45 
+            return "Must be above 45 pounds!"
+        elsif input.to_i % 5 != 0
+            return "Must be divisible by 5!" 
+        else
+          plate_hash = {}
+          plate_array = [45, 35, 25, 10, 5, 2.5]
+          one_side_weight = (input.to_i - 45) / 2.0
+        
+          plate_array.each do |plate_size|
+              plate_amount = (one_side_weight / plate_size).floor
+              plate_hash[plate_size.to_s.to_sym] = plate_amount
+              one_side_weight -= (plate_size * plate_amount)
+          end 
+        end
+        plate_hash.reject! {|plate, amount| amount == 0}
     end
 end
